848c71410976bb281f0ad1f6aec3e99fd00e8863
SebaMoralesG <srmoralesg@gmail.com>
2022-11-10 11:06:07 -0300

    API request for CEN Power plants

--- transmition_CEN.py
+++ transmition_CEN.py
@@ -2,6 +2,8 @@ import pandas as pd
 import requests
 import re
 from time import process_time
+import math
+from sklearn.cluster import KMeans
 
 
 def GET_API_SIP(url, token, date: str | None = None, offset = 0, limit = 20):
@@ -59,6 +61,56 @@ def exectime(file,flag,t0):
         print("Execution Time: ",t1-t0," sec.")
         print("______________________________________________")
         return 0
+
+def utmToLatLong(utmNorthing, utmEasting, utmZone):
+    eastingOffset = 500000.0
+    northingOffset = 10000000.0
+    k0 = 0.9996
+    equatorialRadius = 6378137.0
+    eccSquared = 0.006694380023
+    eccPrimeSquared = eccSquared / (1 - eccSquared)
+    e1 = (1 - math.sqrt(1 - eccSquared)) / (1 + math.sqrt(1 - eccSquared));
+    rad2deg = 180.0/math.pi
+
+    # Casts input from string to floats or ints
+    # Removes 500,000 metre offset for longitude
+    xUTM = float(utmEasting) - eastingOffset
+    yUTM = float(utmNorthing) - northingOffset
+    zoneNumber = int(utmZone)
+
+    # This line below is for debug purposes only, remove for batch processes.
+    # print('The input is: ' + str(utmEasting) + 'm E, ' + str(utmNorthing) + 'm N in NAD83 UTM Zone ' + str(utmZone) + '\n')
+
+    # Finds the origin longitude for the zone
+    lonOrigin = (zoneNumber - 1) * 6 - 180 + 3 # +3 puts in zone centre
+
+    M = yUTM / k0 #This finds the meridional arc
+    mu = M / (equatorialRadius * (1- eccSquared / 4 - 3 * eccSquared * eccSquared / 64 -5 * eccSquared * eccSquared * eccSquared /256))
+
+    # Calculates the footprint latitude
+    phi1Rad = mu + (3 * e1 / 2 - 27 * e1 * e1 * e1 /32) * math.sin(2*mu) + ( 21 * e1 * e1 / 16 - 55 * e1 * e1 * e1 * e1 / 32) * math.sin( 4 * mu) + (151 * e1 * e1 * e1 / 96) * math.sin(6 * mu)
+    phi1 = phi1Rad * rad2deg
+
+    # Variables for conversion equations
+    N1 = equatorialRadius / math.sqrt( 1 - eccSquared * math.sin(phi1Rad) *  math.sin(phi1Rad))
+    T1 = math.tan(phi1Rad) * math.tan(phi1Rad)
+    C1 = eccPrimeSquared * math.cos(phi1Rad) * math.cos(phi1Rad)
+    R1 = equatorialRadius * (1 - eccSquared) / math.pow(1 - eccSquared * math.sin(phi1Rad) * math.sin(phi1Rad), 1.5)
+    D = xUTM / (N1 * k0)
+
+    # Calculate latitude, in decimal degrees
+    lat = phi1Rad - ( N1 * math.tan(phi1Rad) / R1) * (D * D / 2 - (5 + 3 * T1 + 10 * C1 - 4 * C1 * C1 - 9 * eccPrimeSquared) * D * D * D * D / 24 + (61 + 90 * T1 + 298 * C1 + 45 * T1 * T1 - 252 * eccPrimeSquared - 3 * C1 * C1) * D * D * D * D * D * D / 720)
+    lat = lat * rad2deg
+    
+    # Calculate longitude, in decimal degrees
+    lon = (D - (1 + 2 * T1 + C1) * D * D * D / 6 + (5 - 2 * C1 + 28 * T1 - 3 * C1 * C1 + 8 * eccPrimeSquared + 24 * T1 * T1) * D * D * D * D * D / 120) / math.cos(phi1Rad)
+    lon = lonOrigin + lon * rad2deg
+
+    # Print function below is for debug purposes
+    # Note: THIS IS THE LOCATION WHERE THE NUMBERS ARE ROUNDED TO 5 DECIMAL PLACES
+    # print( "Lat: " + str(round(lat, 5)) + ", Long: " + str(round(lon,5)))
+    
+    return lat, lon
 
 
 ## CEN Buses
